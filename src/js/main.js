9cdc7bbc49a77bec81a0c1bf4da3c50ddee92988
Konstantin <zukaaman@gmail.com>
2018-03-22 15:36:22 +0400

    ebanoe.it

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+/*
+ * Custom
+ */
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  //= ../../node_modules/jquery/dist/jquery.js
+
+  //= ../../node_modules/bootstrap/dist/js/bootstrap.js
+});
