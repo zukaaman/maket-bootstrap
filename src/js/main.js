f00a8a1aa5dd9178bdf34aa4472e7c45ef6fbe09
Konstantin <zukaaman@gmail.com>
2018-03-22 18:50:36 +0400

    ebanoe.ai

--- src/js/main.js
+++ src/js/main.js
@@ -6,4 +6,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
   //= ../../node_modules/jquery/dist/jquery.js
 
   //= ../../node_modules/bootstrap/dist/js/bootstrap.js
+
+  //= partials/slider.js
 });
