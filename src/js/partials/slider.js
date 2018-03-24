63fe1cda5c25e8147b50df192b8785da7963886f
Konstantin <zukaaman@gmail.com>
2018-03-24 18:51:12 +0400

    all done

--- src/js/partials/slider.js
+++ src/js/partials/slider.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
   $('.top__slider').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
     arrows: true,
     dots: true,
     prevArrow: '<button class="top__slider-button-left" type="button"><span class="visually-hidden">left</span></button>',
