e55a0eafc7cb2827808e05c1e55dbe1ede1df817
Konstantin <zukaaman@gmail.com>
2018-03-31 20:07:34 +0400

    last fixes

--- src/js/main.js
+++ src/js/main.js
@@ -3,10 +3,6 @@
  */
 
 document.addEventListener("DOMContentLoaded", function(event) {
-  //= ../../node_modules/jquery/dist/jquery.js
-
-  //= ../../node_modules/slick-carousel/slick/slick.js
-
   //= partials/menu.js
 
   //= partials/search.js
